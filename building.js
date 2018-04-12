12cdb7d24c57105ded95b1abeec8afba52bea52a
Jakub Nowakowski <jakub.nowakowski@elpassion.pl>
2018-04-12 17:52:50 +0200

    Change building height on click

--- building.js
+++ building.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { asset, Box, Plane } from 'react-vr';
+import { asset, Box, Plane, VrButton } from 'react-vr';
 
 export class Building extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { height: 1 }
+    this.changeHeight = this.changeHeight.bind(this);
   }
 
   style() {
@@ -11,23 +13,31 @@ export class Building extends React.Component {
     return {
       transform: [
         { translateX: transform.translateX },
-        { translateY: transform.translateY + this.props.data.dimensions.height / 2},
+        { translateY: transform.translateY + this.state.height / 2},
         { translateZ: transform.translateZ },
         { rotateY: transform.rotateY }
       ]
     }
   }
 
+  changeHeight() {
+    this.setState({
+      height: this.props.data.dimensions.height
+    })
+  }
+
   render() {
     const dimensions = this.props.data.dimensions
     return (
-      <Box
-        style={this.style()}
-        dimWidth={dimensions.width}
-        dimHeight={dimensions.height}
-        dimDepth={dimensions.depth}
-        texture={asset('test_texture.png')}
-      />
+      <VrButton onClick={this.changeHeight}>
+        <Box
+          style={this.style()}
+          dimWidth={dimensions.width}
+          dimHeight={this.state.height}
+          dimDepth={dimensions.depth}
+          texture={asset('test_texture.png')}
+        />
+      </VrButton>
     )
   }
 }
