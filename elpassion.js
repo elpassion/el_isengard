da01d0546d149a759cf87e224d9aa1697d818fc3
Jakub Nowakowski <jakub.nowakowski@elpassion.pl>
2018-07-05 09:54:30 +0200

    Fix el passion sign

--- elpassion.js
+++ elpassion.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Text } from 'react-vr';
+import { Text, Plane } from 'react-vr';
 
 export class ElPassion extends React.Component {
   render() {
     return (
-      <Text
-        style={{
-          backgroundColor: '#777879',
-          fontSize: 10,
-          fontWeight: '400',
-          layoutOrigin: [0.5, 0.5],
-          paddingLeft: 0.2,
-          paddingRight: 0.2,
-          textAlign: 'center',
-          textAlignVertical: 'center',
-          transform: [{translate: [0, -135, 30]}, {rotateX: 90}, {rotateY: 180}, {rotateZ: 187}],
-        }}>
-        EL Passion - Hackathon 14.04.2018
-      </Text>
+      <Plane
+        style={{transform: [{translate: [0, -135, 30]}, {rotateX: 90}, {rotateY: 180}, {rotateZ: 187}]}}
+      />
     )
   }
 }
